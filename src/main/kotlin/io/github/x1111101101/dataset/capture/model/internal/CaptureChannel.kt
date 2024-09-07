aff499324dbdbc9bd326b91b9999f6fec4798e6a
Sangyeob <x1111101101@gmail.com>
2024-09-07 21:23:35 +0900

    .gitignore

--- src/main/kotlin/io/github/x1111101101/dataset/capture/model/internal/CaptureChannel.kt
+++ src/main/kotlin/io/github/x1111101101/dataset/capture/model/internal/CaptureChannel.kt
@@ -2,6 +2,6 @@ package io.github.x1111101101.dataset.capture.model.internal
 
 class CaptureChannel(val id: Int) {
 
-
+    val workers = ArrayList<Int>()
 
 }
--- src/main/kotlin/io/github/x1111101101/dataset/capture/model/public/CaptureSession.kt
+++ src/main/kotlin/io/github/x1111101101/dataset/capture/model/public/CaptureSession.kt
@@ -3,6 +3,10 @@ package io.github.x1111101101.dataset.capture.model.public
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class CaptureSession(val id: String, val shots: List<CaptureShot>) {
+data class CaptureSession(
+    val id: String,
+    val shots: List<CaptureShot>,
+    val time: String
+) {
 
 }
